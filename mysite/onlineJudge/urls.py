f30c3f3474da9dda2181d6499bbd3ccc22ea9046
ronalen <ritikroylen75@gmail.com>
2022-07-19 12:41:25 +0530

    updated urls page

--- mysite/onlineJudge/urls.py
+++ mysite/onlineJudge/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+app_name = 'onlineJudge'
 
 urlpatterns = [
     # path('', views.IndexView.as_view(), name='index')
     path('', views.Index, name='index'),
     path('<int:problem_id>/', views.Detail, name='detail'),
+    path('<int:problem_id>/submit/', views.submitProblem, name='submit'),
 ]
